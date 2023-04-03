84b3a7b998a7314a76e9a288094edeafb5d1d09f
kappys <alexsbd1@gmail.com>
2023-04-03 17:15:11 +0200

    feat(): add 3 projects for example

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,10 +9,25 @@ const blogSchema = z.object({
 	badge: z.string().optional()
 })
 
+const projectSchema = z.object({
+	title: z.string(),
+	description: z.string(),
+	pubDate: z.coerce.date(),
+	updatedDate: z.string().optional(),
+	heroImage: z.string().optional(),
+	badge: z.string().optional(),
+	technologies: z
+		.array(z.object({ id: z.string(), name: z.string(), src: z.string() }))
+		.optional()
+})
+
 export type BlogSchema = z.infer<typeof blogSchema>
+export type ProjectSchema = z.infer<typeof projectSchema>
 
 const blogCollection = defineCollection({ schema: blogSchema })
+const projectCollection = defineCollection({ schema: projectSchema })
 
 export const collections = {
-	blog: blogCollection
+	blog: blogCollection,
+	project: projectCollection
 }
---
+++ src/content/project/cloudy-optimizer/index.md
@@ -0,0 +1,13 @@
+---
+title: "Cloudy optimizer"
+description: "Free tool that helps you to analyze your website and optimize your images with cloudinary."
+pubDate: "Apr, 04 2023"
+heroImage: "/project/cloudy-optimizer/home.avif"
+technologies: [
+	{id: "html5", name: "HTML 5", src: "icons/html5.svg" },  
+	{id: "tailwind", name: "Tailwind CSS", src: "icons/tailwind.svg"},
+	{id: "typescript", name: "Typescript", src: "icons/typescript.svg" },  
+	{id: "next", name: "Next js", src: "icons/nextjs.svg" }
+]
+---
+
